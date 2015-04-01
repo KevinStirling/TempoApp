3c44915a235099497f35bf4119cf9e56fbbf8396
bscalera <Scalerab1@mail.montclair.edu>
2015-04-01 12:16:47 -0400

    Add sms code
    Send a text message to be able to vote

--- TempoApp/app/src/main/java/radtech/tempoapp/FragmentB.java
+++ TempoApp/app/src/main/java/radtech/tempoapp/FragmentB.java
@@ -14,4 +14,15 @@ public class FragmentB extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_b,container,false);
     }
+    
+    
+    public void invokeSMSApp() {
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+
+        smsIntent.putExtra("sms_body", "Hello World!"); 
+        smsIntent.putExtra("address", "0123456789");
+        smsIntent.setType("vnd.android-dir/mms-sms");
+
+        startActivity(smsIntent);
+   }
 }
