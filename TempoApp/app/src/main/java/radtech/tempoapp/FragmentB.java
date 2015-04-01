37853bc0092fd4629d9836eed6178a5ac7d503b0
KevinStirling <youngks93@gmail.com>
2015-04-01 13:09:55 -0400

    changing color scheme

--- TempoApp/app/src/main/java/radtech/tempoapp/FragmentB.java
+++ TempoApp/app/src/main/java/radtech/tempoapp/FragmentB.java
@@ -1,10 +1,20 @@
 package radtech.tempoapp;
 
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 
 /**
  * Created by kevinyoung on 2/22/15.
@@ -12,6 +22,11 @@ import android.view.ViewGroup;
 public class FragmentB extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_b,container,false);
     }
+
+
+
 }
