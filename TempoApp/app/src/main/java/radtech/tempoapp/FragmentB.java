1f55d24ff87754782169d951be33c48bdb03573e
Pat Needham <needham.patrick@gmail.com>
2015-05-01 18:45:00 -0400

    Update CCC vote page
    Include AsyncTask which parses the live vote page on website
    (http://www.gottempo.com/home/shows/ccc.php#top) to select the current
    music videos dynamically and use those values to update the fragment
    list

--- TempoApp/app/src/main/java/radtech/tempoapp/FragmentB.java
+++ TempoApp/app/src/main/java/radtech/tempoapp/FragmentB.java
@@ -3,8 +3,12 @@ package radtech.tempoapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by kevinyoung on 2/22/15.
@@ -21,6 +37,7 @@ import android.widget.Toast;
 
 public class FragmentB extends ListFragment {
     private boolean isLoggedIn = false;
+    private ArrayAdapter<String> adapter;
 
     //THE onCreateView METHOD SETS UP THE VIEW FOR THE FRAGMENT. EVERYTHING THAT IS DISPLAYED ON THE SCREEN PHYSICALLY IS DECLARED HERE
     @Override
@@ -31,10 +48,15 @@ public class FragmentB extends ListFragment {
         String[] values = new String[] { "Music Video 1", "Music Video 2", "Music Video 3",
                 "Music Video 3", "Music Video 4", "Music Video 5", "Music Video 6", "Music Video 7",
                 "Music Video 8", "Music Video 9", "Music Video 10" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
 
+        new RetrieveVideosTask().execute();
+
+        ArrayList<String> valuesList = new ArrayList<String>();
+        valuesList.addAll(Arrays.asList(values));
+
+        adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, valuesList);
+        setListAdapter(adapter);
         final Button loginbutton = (Button) root.findViewById(R.id.loginbutton);
 
         loginbutton.setOnClickListener(new View.OnClickListener() {
@@ -73,5 +95,69 @@ public class FragmentB extends ListFragment {
         smsIntent.putExtra("sms_body", "I vote for "+data);
         smsIntent.putExtra("exit_on_sent", true);
         startActivity(smsIntent);
-   }
+    }
+
+
+    private class RetrieveVideosTask extends AsyncTask<String, Void, ArrayList<String>> {
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+
+            BufferedReader reader;
+            StringBuilder sb;
+
+            try {
+                HttpClient httpclient = new DefaultHttpClient(); // Create HTTP Client
+                HttpGet httpget = new HttpGet("http://www.gottempo.com/home/shows/ccc.php#top"); // Set the action you want to do
+                HttpResponse response = httpclient.execute(httpget); // Executeit
+                HttpEntity entity = response.getEntity();
+                InputStream is = entity.getContent(); // Create an InputStream with the response
+                reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) // Read line by line
+                    sb.append(line + "\n");
+
+                ArrayList<String> musicVideoNames = new ArrayList<String>();
+                String resString = sb.toString(); // Result is here
+
+                int loopIndex = resString.indexOf("name=\"poll_choice\"");
+                int carotIndex, endIndex;
+                String musicVideoName;
+                Spanned spanned;
+                while (loopIndex != -1) {
+                    carotIndex = resString.indexOf(">", loopIndex);
+                    endIndex = resString.indexOf("</li>", loopIndex);
+                    musicVideoName = resString.substring(carotIndex + 2, endIndex);
+                    spanned = Html.fromHtml(musicVideoName);
+                    musicVideoName = spanned.toString();
+
+                    Log.d("current video: ", musicVideoName);
+                    musicVideoNames.add(musicVideoName);
+                    loopIndex = resString.indexOf("name=\"poll_choice\"", endIndex);
+                }
+
+                is.close(); // Close the stream
+                return musicVideoNames;
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+                Log.d("IOException", e.toString());
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(ArrayList<String> results) {
+            super.onPostExecute(results);
+            if (results != null) {
+                try {
+                    adapter.clear();
+                    adapter.addAll(results);
+                } catch (java.lang.UnsupportedOperationException e) {
+                    Log.d("unsupportedOperation", e.toString());
+                }
+
+            }
+        }
+    }
 }
