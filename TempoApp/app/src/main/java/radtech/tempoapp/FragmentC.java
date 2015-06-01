5d1fee7205642363bbb755204f49d52ee01a62cd
Pat Needham <needham.patrick@gmail.com>
2015-06-01 17:06:38 -0400

    Improve Fragment C's text view
    Fragment C’s text view now updates every time one of the number (0-9)
    or special character (* or #) buttons are clicked. The Clear button
    also works as intended by clearing whatever is currently inside the
    text view. The only thing left is to get the call button to start a
    call, either by starting a new activity using the default phone dialer,
    or with VOIP API.

--- TempoApp/app/src/main/java/radtech/tempoapp/FragmentC.java
+++ TempoApp/app/src/main/java/radtech/tempoapp/FragmentC.java
@@ -1,5 +1,7 @@
 package radtech.tempoapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -24,7 +26,7 @@ public class FragmentC extends Fragment implements View.OnClickListener {
 
         View v = inflater.inflate(R.layout.fragment_c, container, false);
         phoneAddressTextView = (TextView) v.findViewById(R.id.phoneAddressTextView);
-        numberButtons = new Button[10];
+        numberButtons = new Button[14];
         numberButtons[1] = (Button) v.findViewById(R.id.button2);
         numberButtons[2] = (Button) v.findViewById(R.id.button3);
         numberButtons[3] = (Button) v.findViewById(R.id.button4);
@@ -36,6 +38,12 @@ public class FragmentC extends Fragment implements View.OnClickListener {
         numberButtons[9] = (Button) v.findViewById(R.id.button10);
         numberButtons[0] = (Button) v.findViewById(R.id.button12);
 
+        numberButtons[10] = (Button) v.findViewById(R.id.button11); //*
+        numberButtons[11] = (Button) v.findViewById(R.id.button13); //#
+        numberButtons[12] = (Button) v.findViewById(R.id.button15); //Call
+        numberButtons[13] = (Button) v.findViewById(R.id.button15); //End Call
+
+
         for (Button b : numberButtons) {
             if (b != null) {
                 b.setOnClickListener(this);
@@ -46,18 +54,22 @@ public class FragmentC extends Fragment implements View.OnClickListener {
         }
 
 
-        return inflater.inflate(R.layout.fragment_c,container,false);
-    }
-
-    public void onButtonClick(View v) {
-
+        return v;
     }
 
     @Override
     public void onClick(View v) {
-        Log.v("inside onButtonClick", "double yay");
-        String newValue = (String) phoneAddressTextView.getText() + ((Button) v).getText();
-        phoneAddressTextView.setText(newValue);
-        Log.v("finished onButtonClick", "double yay!!!!!");
+        String text = (String) ((Button) v).getText();
+        if (text.equals("CLEAR")) {
+            phoneAddressTextView.setText("");
+        } else if (text.equals("CALL")) {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + phoneAddressTextView.getText()));
+            getActivity().startActivity(intent);
+        } else {
+            String newValue = (String) phoneAddressTextView.getText() + text;
+            phoneAddressTextView.setText(newValue);
+        }
+
     }
 }
